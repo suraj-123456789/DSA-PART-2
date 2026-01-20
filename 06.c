214ede5a016afca19f6909ba5d2c1a92b9f6ee3f
suraj-123456789 <surajvishwakarma4588@gmail.com>
2026-01-20 19:18:15 +0530

    changes added

---
+++ 06.c
@@ -0,0 +1,17 @@
+fubfegbo9VO i9gf9gb
+sgv
+aqg
+agb
+heh
+__BIGGEST_ALIGNMENT__b
+aeqhbae
+break;
+
+__DBL_HAS_QUIET_NAN__a
+break;axb
+
+break;break;alignasb
+__has_attributeb
+ahb
+__DBL_HAS_QUIET_NAN__be
+break;break;
